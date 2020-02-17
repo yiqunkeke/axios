ea26811212b42ab486ad4c9b31a757b21596c067
unknown <1431806069@qq.com>
2020-02-17 11:42:57 +0800

    vant的安装与引入

--- axios-code/src/views/About.vue
+++ axios-code/src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <van-button type="primary">主要按钮</van-button>
   </div>
 </template>
+
+<script>
+import { Button } from 'vant';
+
+export default {
+  name: 'vantDemo',
+  components: {
+    [Button.name]: Button
+  }
+}
+</script>
